8ef687e196ce07316a8b319dd23215abdee86c43
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-05-23 18:05:16 +0530

    Update fibbanoci_series.cpp

--- fibbanoci_series.cpp
+++ fibbanoci_series.cpp
@@ -1,3 +1,5 @@
+//code without using arrays
+
 #include <iostream>
 using namespace std;
 int main() {
@@ -17,4 +19,22 @@ int main() {
    }
 
     return 0;
+}
+
+//code using arrays
+#include <iostream>
+using namespace std;
+int main() {
+   int n,i;
+   cin>>n;
+  int a[n];
+  a[0]=0;
+  a[1]=1;
+  cout<<a[0]<<" ";
+  cout<<a[1]<<" ";
+  for(i=2;i<n;i++){
+      a[i]=a[i-2]+a[i-1];
+      cout<<a[i]<<" ";
+  }
+  
 }
