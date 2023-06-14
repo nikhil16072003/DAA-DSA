8dcf037a2f27f330a83ae3ba6b1217a60e6380c6
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-06-14 20:27:37 +0530

    Update fibbanocii_series-using_recursion.cpp

--- fibbanocii_series-using_recursion.cpp
+++ fibbanocii_series-using_recursion.cpp
@@ -25,3 +25,22 @@ int main() {
 
     return 0;
 }
+/////////////////////////////////////////////////////////////////
+/*
+#include<iostream>
+using namespace std;
+int fib(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    int sum=fib(n-1)+fib(n-2);
+    return sum;
+}
+int main(){
+    int n,i;
+    cin>>n;
+   for(i=1;i<=n;i++){
+       cout<<fib(i)<<" ";
+   }
+}
+*/
