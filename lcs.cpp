ba46aebafd368a6e11b52b0fbd1f92b66ddf46a9
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-07-11 20:30:22 +0530

    Update lcs.cpp

--- lcs.cpp
+++ lcs.cpp
@@ -1,37 +1,51 @@
 #include <cstring>
 #include <iostream>
+#include<algorithm>
 using namespace std;
 void lcsAlgo(char *S1, char *S2, int m, int n) {
-  int LCS_table[m + 1][n + 1];
-  for (int i = 0; i <= m; i++) {
-    for (int j = 0; j <= n; j++) {
-      if (i == 0 || j == 0)
-        LCS_table[i][j] = 0;
-      else if (S1[i - 1] == S2[j - 1])
-        LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1;
-      else
-        LCS_table[i][j] = max(LCS_table[i - 1][j], LCS_table[i][j - 1]);
-    }
-  }
-int index = LCS_table[m][n];
-char lcsAlgo[index + 1];
-lcsAlgo[index] = '\0';
-int i = m, j = n;
-  while (i > 0 && j > 0) {
-    if (S1[i - 1] == S2[j - 1]) {
-      lcsAlgo[index - 1] = S1[i - 1];
-      i--;
-      j--;
-      index--;
-    }
-
-    else if (LCS_table[i - 1][j] > LCS_table[i][j - 1])
-      i--;
-    else
-      j--;
-  }
- 
-  cout << "S1 : " << S1 << "\nS2 : " << S2 << "\nLCS: " << lcsAlgo << "\n";
+    int i,j;
+ int lcs_mat[m+1][n+1];
+ cout<<"The Lcs matrix is;\n";
+ for( i=0;i<=m;i++){
+     for(j=0;j<=n;j++){
+         if(i==0 || j==0){
+             lcs_mat[i][j]=0;
+         }
+         else if(S1[i-1]==S2[j-1]){
+             lcs_mat[i][j]=lcs_mat[i-1][j-1]+1;
+         }
+         else{
+             lcs_mat[i][j]=max(lcs_mat[i-1][j],lcs_mat[i][j-1]);
+         }
+     }
+ }
+ for(i=0;i<=m;i++){
+     for(j=0;j<=n;j++){
+         cout<<lcs_mat[i][j]<<" ";
+     }
+     cout<<endl;
+ }
+ int index=lcs_mat[m][n];
+ char lcs[index+1];
+ i=m;
+ j=n;
+ lcs[index]='\0';
+ while(i>0 && j>0){
+     if(S1[i-1]==S2[j-1]){
+         lcs[index-1]=S1[i-1];
+         i--;
+         j--;
+         index--;
+         
+     }
+     else if(lcs_mat[i-1][j]>lcs_mat[i][j-1]){
+         i--;
+     }
+     else{
+         j--;
+     }
+ }
+ cout<<lcs;
 }
 
 int main() {
